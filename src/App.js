0f223fdfb4ef03c001b9417b96e48d4fdb743669
balazs-varga <balazsvarga91@gmail.com>
2018-11-25 16:23:19 +0100

    create getWeather function to get data from openweather api

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import Titles from './components/Titles';
 import Form from './components/Form';
 import Weather from './components/Weather';
 
+const API_KEY = '2c636f3f514cff271ddd2ba545e59fd0';
+
 class App extends React.Component {
+  getWeather = async () => {
+    const apiCall = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}`);
+    const data = await apiCall.json();
+  }
+
   render() {
     return (
       <div>
