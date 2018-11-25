de76da1fe0cb5c3998243ab69cec0b4bcd0f8301
balazs-varga <balazsvarga91@gmail.com>
2018-11-25 16:45:33 +0100

    add states to App.js and set states to json data

--- src/App.js
+++ src/App.js
@@ -6,12 +6,29 @@ import Weather from './components/Weather';
 const API_KEY = '2c636f3f514cff271ddd2ba545e59fd0';
 
 class App extends React.Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
+
   getWeather = async (e) => {
     e.preventDefault();
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
     const apiCall = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
     const data = await apiCall.json();
+    this.setState({
+      temperature: data.main.temp,
+      city: data.name,
+      country: data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+      error: ""
+    })
   }
 
   render() {
