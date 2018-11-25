9648739e84aef12caf25fd54bd1bdd6be81760a9
balazs-varga <balazsvarga91@gmail.com>
2018-11-25 17:02:05 +0100

    display error message when values are not given

--- src/App.js
+++ src/App.js
@@ -21,13 +21,23 @@ class App extends React.Component {
     const country = e.target.elements.country.value;
     const apiCall = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
     const data = await apiCall.json();
-    this.setState({
-      temperature: data.main.temp,
-      city: data.name,
-      country: data.sys.country,
-      humidity: data.main.humidity,
-      description: data.weather[0].description,
-      error: ""
+    if (city && country) {
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
+      })
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: "Please enter the value."
     })
   }
 
@@ -35,8 +45,8 @@ class App extends React.Component {
     return (
       <div>
         <Titles />
-        <Form getWeather={this.getWeather}/>
-        <Weather 
+        <Form getWeather={this.getWeather} />
+        <Weather
           temperature={this.state.temperature}
           city={this.state.city}
           country={this.state.country}
